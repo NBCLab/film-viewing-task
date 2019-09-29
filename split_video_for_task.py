0d003d91097142aa09a36b183e14c4360defe482
Taylor Salo <tsalo006@fiu.edu>
2019-09-29 13:19:44 -0400

    Drop title credits and ignore SRTs for now.

--- split_video_for_task.py
+++ split_video_for_task.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 """
 Scene detection:
-scenedetect --input S01E04.mp4 --stats S01E04_stats/S01E04_stats.csv detect-content --threshold 27 list-scenes
+scenedetect --input ../S01E05.m4v --stats S01E05_stats.csv detect-content --threshold 27 list-scenes
 """
 
 import os
@@ -15,17 +15,38 @@ import numpy as np
 import pandas as pd
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-SPLIT_TIMES = {'S01E01': [5., 505.5, 968.4, 1380.26, 1808.85, 2275.44, 2774.1],
-               'S01E02': [5.5, 524.05, 897.75, 1440.55, 1971.305, 2412.293, 2738.42525, 3179.2],
-               'S01E03': [5.5, 462.504, 927.51, 1402.526, 1940.856, 2342.173, 2966.],
-               'S01E04': [5.5, 465.256, 958.207, 1397.354, 1896.895, 2240.905, 2883.881],
-               }
-# My times
-SPLIT_TIMES = {'S01E01': [5., 505.5, 968.4, 1380.26, 1808.85, 2275.44, 2774.1],
-               'S01E02': [5.5, 524.05, 897.75, 1440.55, 1971.305, 2412.293, 2738.42525, 3179.2],
-               'S01E03': [5.5, 462.49, 927.48, 1402.5075, 1940.835, 2342.17, 2966.],
-               'S01E04': [5.5, 465.5, 958.2, 1397.32, 1896.8, 2240.9, 2886.],
-               }
+SPLIT_TIMES = {'S01E01': [(5., 505.5),
+                          (555.5, 968.4),
+                          (968.4, 1380.26),
+                          (1380.26, 1808.85),
+                          (1808.5, 2275.44),
+                          (2275.44, 2774.1)],
+               'S01E02': [((5.5, 275.), (325., 524.05)),
+                          (524.05, 897.75),
+                          (897.75, 1440.55),
+                          (1440.55, 1971.305),
+                          (1971.305, 2412.293),
+                          (2412.293, 2738.42525),
+                          (2738.42525, 3179.2)],
+               'S01E03': [((5.5, 121.), (171., 462.49)),
+                          (462.49, 927.48),
+                          (927.48, 1402.5075),
+                          (1402.5075, 1940.835),
+                          (1940.835, 2342.17),
+                          (2342.17, 2966.)],
+               'S01E04': [(5.5, 465.5),
+                          (515.5, 958.2),
+                          (958.2, 1397.32),
+                          (1397.32, 1896.8),
+                          (1896.8, 2240.9),
+                          (2240.9, 2886.)],
+               'S01E05': [(5.5, 495.),
+                          (495., 545.),
+                          (545., 986.277),
+                          (986.277, 1509.425),
+                          (1509.425, 1950.532),
+                          (1950.532, 2462.585),
+                          (2462.585, 3038.5)]}
 
 
 def run(command, env={}):
@@ -50,14 +71,13 @@ def run(command, env={}):
 
 def split_video(in_file):
     mp4_file = in_file.replace('.mov', '.mp4')
-    srt_file = in_file.replace('.mov', '.srt')
     fname = op.splitext(op.basename(in_file))[0]
     out_dir = op.dirname(in_file)
     clips_dir = op.join(out_dir, fname)
     if not op.isdir(out_dir):
         mkdir(out_dir)
 
-    split_times = SPLIT_TIMES[fname]
+    episode_split_times = SPLIT_TIMES[fname]
 
     if op.isdir(clips_dir):
         print('{0} exists. Skipping.'.format(clips_dir))
@@ -71,62 +91,63 @@ def split_video(in_file):
                '{full_mp4}').format(full_mov=in_file, full_mp4=mp4_file)
         run(cmd)
 
-    # Split SRT file
-    print('\n\n\nSplitting SRT file\n')
-    cmd = ('srt split {times} {srt_file}').format(
-        times=' '.join(['{}s'.format(t) for t in split_times]),
-        srt_file=srt_file)
-    print(cmd+'\n\n\n')
-    run(cmd)
-
-    srt_search_term = srt_file.replace('.srt', '.*.srt')
-    srt_files = sorted(glob(srt_search_term))
-
-    for i_run, start_time in enumerate(split_times[:-1]):
+    for i_run, split_times in enumerate(episode_split_times[:-1]):
         run_file = op.join(clips_dir, 'uc{0}R{1:02d}.mp4'.format(fname, i_run+1))
         run_file_drc = op.join(clips_dir, '{0}R{1:02d}.mp4'.format(fname, i_run+1))
-        run_srt_file = op.join(clips_dir, '{0}R{1:02d}.srt'.format(fname, i_run+1))
+        if op.isfile(run_file):
+            print('Skipping {0}R{1:02d}. Already exists.'.format(fname, i_run+1))
+            continue
 
-        sel_srt_file = [f for f in srt_files if '.{}.srt'.format(i_run+2) in f][0]
-        copyfile(sel_srt_file, run_srt_file)
+        if isinstance(split_times[0], tuple):
+            # split and merge
+            temp_files = ['temp_{}.mp4'.format(j_clip) for j_clip in range(len(split_times))]
 
-        end_time = split_times[i_run+1]
-        dur = end_time - start_time
+            for j_clip, clip_split_times in enumerate(split_times):
+                print('Splitting video')
+                dur = clip_split_times[1] - clip_split_times[0]
+                cmd = ('ffmpeg -ss {start_time} -t {duration} -i {video_file} '
+                       '{clip_file}').format(
+                            start_time=clip_split_times[0],
+                            duration=dur,
+                            video_file=mp4_file,
+                            clip_file=temp_files[j_clip])
+                print(cmd+'\n\n\n')
+                run(cmd)
 
-        # split video
-        if not op.isfile(run_file):
-            print('\n\n\nSplitting scene {}\n'.format(i_run+1))
-            # Quick-and-dirty approach.
-            # Due to encoding, this leads to frozen/missing frames
-            # ffmpeg_extract_subclip(mp4_file, start_time, end_time, targetname=run_file)
+            print('Merging clips')
+            with open('merge_list.txt', 'w') as fo:
+                temp_str = '\n'.join(["file '{}'".format(tf) for tf in temp_files])
+                fo.write(temp_str)
 
-            # Slow-and-accurate (hopefully) approach.
-            # Actually transcodes MP4 to MP4, which should create new I-Frames.
-            cmd = ('ffmpeg -ss {start_time} -t {duration} -i {full_mp4} '
-                   '{split_avi}').format(
-                        start_time=start_time,
+            cmd = 'ffmpeg -f concat -safe 0 -i merge_list.txt -c copy {}'.format(run_file)
+            print(cmd+'\n\n\n')
+            run(cmd)
+
+            print('Removing temporary files')
+            os.remove('merge_list.txt')
+            for tf in temp_files:
+                os.remove(tf)
+        else:
+            # split
+            print('Splitting video')
+            dur = split_times[1] - split_times[0]
+            cmd = ('ffmpeg -ss {start_time} -t {duration} -i {video_file} '
+                   '{clip_file}').format(
+                        start_time=split_times[0],
                         duration=dur,
-                        full_mp4=mp4_file,
-                        split_avi=run_file)
+                        video_file=mp4_file,
+                        clip_file=run_file)
             print(cmd+'\n\n\n')
             run(cmd)
 
-            # crop and add black bars. Not doing this!
-            # dynamic range compression
-            print('\n\n\nPerforming dynamic range compression\n')
-            cmd = ('ffmpeg -i {split_avi} -filter_complex '
-                   '"[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0'
-                   ':-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy '
-                   '{csplit_avi}').format(split_avi=run_file,
-                                          csplit_avi=run_file_drc)
-            print(cmd+'\n\n\n')
-            run(cmd)
-
-        # reencode video to Indeo 5.1 and audio to PCM uncompressed?
-        # Not sure if necessary
-
-    for run_srt_file in srt_files:
-        os.remove(run_srt_file)
+        print('\n\n\nPerforming dynamic range compression\n')
+        cmd = ('ffmpeg -i {clip_file} -filter_complex '
+               '"[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0'
+               ':-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy '
+               '{cclip_file}').format(clip_file=run_file,
+                                      cclip_file=run_file_drc)
+        print(cmd+'\n\n\n')
+        run(cmd)
 
 
 if __name__ == '__main__':
