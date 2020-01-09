1d639e2d69a97cf213d8330bd212ae5b34143019
Taylor Salo <tsalo006@fiu.edu>
2020-01-09 12:00:11 -0500

    Add job files.

---
+++ job_files/sub_S01E05.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#---Number of cores
+#SBATCH -c 1
+
+#---Job's name in SLURM system
+#SBATCH -J S01E05
+
+#---Error file
+#SBATCH -e S01E05
+
+#---Output file
+#SBATCH -o S01E05
+
+#---Queue name
+#SBATCH -q acc_nbc
+
+#---Partition
+#SBATCH -p centos7
+########################################################
+export NPROCS=`echo $LSB_HOSTS | wc -w`
+export OMP_NUM_THREADS=$NPROCS
+. $MODULESHOME/../global/profile.modules
+module load singularity-3
+
+singularity exec /scratch/tsalo006/stranger-things/ffmpeg.sif /scratch/tsalo006/stranger-things/scripts/run_S01E05.sh
