e17b5a618f2e4d68991fabb7214e8cba650d261f
Taylor Salo <tsalo006@fiu.edu>
2020-01-09 11:52:34 -0500

    Make scripts.

--- scripts/run_S01E01.sh
+++ scripts/run_S01E01.sh
@@ -1,60 +1,60 @@
 #Processing S01E01R01
 #Splitting video
-ffmpeg -ss 5.0 -i /home/data/nbc/Laird_DIVA/stimuli/S01E01.mp4 -t 500.5 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R01.mp4
+ffmpeg -ss 5.0 -i /scratch/tsalo006/stranger-things/raw/S01E01.mp4 -t 500.5 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R01.mp4
 
 #Performing dynamic range compression
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R01.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R01.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R01.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R01.mp4
 
 #Downsampling for PsychoPy compatibility
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R01.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/S01E01R01.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R01.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/S01E01R01.mp4
 
 #Processing S01E01R02
 #Splitting video
-ffmpeg -ss 555.5 -i /home/data/nbc/Laird_DIVA/stimuli/S01E01.mp4 -t 412.9 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R02.mp4
+ffmpeg -ss 555.5 -i /scratch/tsalo006/stranger-things/raw/S01E01.mp4 -t 412.9 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R02.mp4
 
 #Performing dynamic range compression
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R02.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R02.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R02.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R02.mp4
 
 #Downsampling for PsychoPy compatibility
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R02.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/S01E01R02.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R02.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/S01E01R02.mp4
 
 #Processing S01E01R03
 #Splitting video
-ffmpeg -ss 968.4 -i /home/data/nbc/Laird_DIVA/stimuli/S01E01.mp4 -t 411.86 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R03.mp4
+ffmpeg -ss 968.4 -i /scratch/tsalo006/stranger-things/raw/S01E01.mp4 -t 411.86 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R03.mp4
 
 #Performing dynamic range compression
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R03.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R03.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R03.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R03.mp4
 
 #Downsampling for PsychoPy compatibility
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R03.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/S01E01R03.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R03.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/S01E01R03.mp4
 
 #Processing S01E01R04
 #Splitting video
-ffmpeg -ss 1380.26 -i /home/data/nbc/Laird_DIVA/stimuli/S01E01.mp4 -t 428.5899999999999 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R04.mp4
+ffmpeg -ss 1380.26 -i /scratch/tsalo006/stranger-things/raw/S01E01.mp4 -t 428.5899999999999 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R04.mp4
 
 #Performing dynamic range compression
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R04.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R04.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R04.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R04.mp4
 
 #Downsampling for PsychoPy compatibility
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R04.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/S01E01R04.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R04.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/S01E01R04.mp4
 
 #Processing S01E01R05
 #Splitting video
-ffmpeg -ss 1808.5 -i /home/data/nbc/Laird_DIVA/stimuli/S01E01.mp4 -t 466.94000000000005 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R05.mp4
+ffmpeg -ss 1808.5 -i /scratch/tsalo006/stranger-things/raw/S01E01.mp4 -t 466.94000000000005 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R05.mp4
 
 #Performing dynamic range compression
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R05.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R05.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R05.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R05.mp4
 
 #Downsampling for PsychoPy compatibility
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R05.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/S01E01R05.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R05.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/S01E01R05.mp4
 
 #Processing S01E01R06
 #Splitting video
-ffmpeg -ss 2275.44 -i /home/data/nbc/Laird_DIVA/stimuli/S01E01.mp4 -t 498.65999999999985 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R06.mp4
+ffmpeg -ss 2275.44 -i /scratch/tsalo006/stranger-things/raw/S01E01.mp4 -t 498.65999999999985 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R06.mp4
 
 #Performing dynamic range compression
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/nondrc_S01E01R06.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R06.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/nondrc_S01E01R06.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R06.mp4
 
 #Downsampling for PsychoPy compatibility
-ffmpeg -i /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/drc_S01E01R06.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /home/data/nbc/Laird_DIVA/tasks/stranger-things-task/temp/S01E01/S01E01R06.mp4
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/drc_S01E01R06.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E01/S01E01/S01E01R06.mp4
 
---
+++ scripts/run_S01E02.sh
@@ -0,0 +1,76 @@
+#Processing S01E02R01
+#Splitting video
+ffmpeg -ss 5.5 -i /scratch/tsalo006/stranger-things/raw/S01E02.mp4 -t 269.5 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/temp_S01E02R01_0.mp4
+
+#Splitting video
+ffmpeg -ss 325.0 -i /scratch/tsalo006/stranger-things/raw/S01E02.mp4 -t 199.04999999999995 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/temp_S01E02R01_1.mp4
+
+#Merging clips
+ffmpeg -f concat -safe 0 -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/S01E02R01_merge_list.txt -c copy /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R01.mp4
+
+#Performing dynamic range compression
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R01.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R01.mp4
+
+#Downsampling for PsychoPy compatibility
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R01.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/S01E02R01.mp4
+
+#Processing S01E02R02
+#Splitting video
+ffmpeg -ss 524.05 -i /scratch/tsalo006/stranger-things/raw/S01E02.mp4 -t 373.70000000000005 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R02.mp4
+
+#Performing dynamic range compression
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R02.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R02.mp4
+
+#Downsampling for PsychoPy compatibility
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R02.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/S01E02R02.mp4
+
+#Processing S01E02R03
+#Splitting video
+ffmpeg -ss 897.75 -i /scratch/tsalo006/stranger-things/raw/S01E02.mp4 -t 542.8 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R03.mp4
+
+#Performing dynamic range compression
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R03.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R03.mp4
+
+#Downsampling for PsychoPy compatibility
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R03.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/S01E02R03.mp4
+
+#Processing S01E02R04
+#Splitting video
+ffmpeg -ss 1440.55 -i /scratch/tsalo006/stranger-things/raw/S01E02.mp4 -t 530.7550000000001 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R04.mp4
+
+#Performing dynamic range compression
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R04.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R04.mp4
+
+#Downsampling for PsychoPy compatibility
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R04.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/S01E02R04.mp4
+
+#Processing S01E02R05
+#Splitting video
+ffmpeg -ss 1971.305 -i /scratch/tsalo006/stranger-things/raw/S01E02.mp4 -t 440.98800000000006 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R05.mp4
+
+#Performing dynamic range compression
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R05.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R05.mp4
+
+#Downsampling for PsychoPy compatibility
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R05.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/S01E02R05.mp4
+
+#Processing S01E02R06
+#Splitting video
+ffmpeg -ss 2412.293 -i /scratch/tsalo006/stranger-things/raw/S01E02.mp4 -t 326.13224999999966 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R06.mp4
+
+#Performing dynamic range compression
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R06.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R06.mp4
+
+#Downsampling for PsychoPy compatibility
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R06.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/S01E02R06.mp4
+
+#Processing S01E02R07
+#Splitting video
+ffmpeg -ss 2738.42525 -i /scratch/tsalo006/stranger-things/raw/S01E02.mp4 -t 440.77475000000004 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R07.mp4
+
+#Performing dynamic range compression
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/nondrc_S01E02R07.mp4 -filter_complex "[0:a]compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2[audio]" -map 0:v -map "[audio]" -codec:v copy /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R07.mp4
+
+#Downsampling for PsychoPy compatibility
+ffmpeg -i /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/drc_S01E02R07.mp4 -codec:v libx264 -crf 0 -preset veryslow -ar 44100 /scratch/tsalo006/stranger-things/processed/S01E02/S01E02/S01E02R07.mp4
+
